1b89e985013514b8973ba35a68bd09580896b2b7
villa-model <164371624+villa-model@users.noreply.github.com>
2024-07-27 23:37:58 -0500

    Fix unescaped entities in TheVilla.tsx

--- pages/TheVilla.tsx
+++ pages/TheVilla.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from "../styles/Villa.module.css";
 import VillaHeader from "./VillaHeader";
 import VillaFooter from "./VillaFooter";
@@ -6,40 +5,34 @@ import VillaFooter from "./VillaFooter";
 const TheVilla: React.FC = () => {
   return (
     <main className={styles.main}>
-      <VillaHeader/>
+        <VillaHeader/>
 
-      <div className={styles.container}>
-        <div className={styles.header}>
-        {/* Video Element */}
-        <div className={styles.videoContainer}>
-          <video
-            className={styles.heroVideo}
-            src="/videos/The_Villa.mp4"
-            autoPlay
-            loop
-            muted
-          />
+        <div className={styles.container}>
+          <div className={styles.header}>
+            <h1 className={styles.title}>
+              THE
+              {" "}
+              <span className={styles.gradientText0}>
+                VILLA
+              </span>
+            </h1>
+            <p className={styles.description}>
+              Welcome!
+            </p>
+           
+            <p className={styles.description}>
+              Some of the coolest projects I&apos;ve seen built in the open. This is The Villa.
+            </p>
+
+            <div className={styles.videoContainer}>
+              <video className={styles.heroVideo} autoPlay loop muted>
+                <source src="/videos/The_Villa.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          </div>
         </div>
-
-          <h1 className={styles.title}>
-            THE
-            {" "}
-            <span className={styles.gradientText0}>
-              VILLA
-            </span>
-          </h1>
-          <p className={styles.description}>
-            Welcome!
-          </p>
-          
-          <p className={styles.description}>
-            Some of the coolest projects I've seen build in the open. This is The Villa.
-          </p>
-        </div>
-
-
-      </div>
-      <VillaFooter/>
+        <VillaFooter/>
     </main>
   );
 };
