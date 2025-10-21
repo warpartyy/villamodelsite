36165224c1818074cc82c2965e8e60379bcf8621
warpartyy <warpartyy@gmail.com>
2025-10-21 14:15:11 +0000

    Removed Thirdweb and Polygon logic from app.tsx

--- Components/config.ts
+++
@@ -1,7 +0,0 @@
-// config.ts
-require('dotenv').config()
-
-export const openSeaApiKey = process.env.OSAPIKey || '';
-export const ThirdWebAPI = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID || '';
-
-export const PRIVATE_KEY = process.env.MMPRIVATE_KEY || '';
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,40 +1,8 @@
 import type { AppProps } from "next/app";
-import { ConnectWallet, MetaMaskWallet, ThirdwebProvider, coinbaseWallet, metamaskWallet, rainbowWallet, walletConnect } from "@thirdweb-dev/react";
 import "../styles/globals.css";
-import { polygon } from "thirdweb/chains";
-require('dotenv').config();
-import { ThirdWebAPI } from '../Components/config';
 
-
-
-const activeChain = "polygon";
-
-function MyApp({ Component, pageProps }: 
-AppProps) {
-
-  return (
-    <ThirdwebProvider
-    activeChain={activeChain}
-    clientId={ThirdWebAPI}
-
-    authConfig={{
-        domain: process.env.DOMAIN ||"",
-        authUrl: "/api/auth"
-     }}
-
-     supportedWallets={[
-        metamaskWallet(),
-        coinbaseWallet(),
-        walletConnect(),
-      ]}
-    >
-      <Component {...pageProps} />
-    </ThirdwebProvider>
-  );
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-
-
 export default MyApp;
-
-
